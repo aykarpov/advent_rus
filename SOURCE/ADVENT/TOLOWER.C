21506dfbc4abf4d14193043c086f2f99282e79cc
Alex Karpov <aykarpov.work@gmail.com>
2021-10-09 14:35:23 +0300

    Add files via upload

---
+++ SOURCE/ADVENT/TOLOWER.C
@@ -0,0 +1,18 @@
+/* …Ž€‡Ž‚€’…‹œ Ž‹œ˜ˆ• “Š‚ ‚ Œ€‹…œŠˆ… */
+
+tolower(adr,lgt)
+    register char *adr;
+    int lgt;
+{
+    register char *oldadr, c;
+
+    oldadr = adr;
+    while( lgt-- ) {
+        c = *adr;
+        if( c>='A' && c<='Z')   *adr |= 040;
+/*      else if( (c&0300)==0300 )  *adr &= ~040; */
+	++adr;
+    }
+
+    return;
+}
